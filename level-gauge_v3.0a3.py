0fe49440cf8012128636d09c2706bd6927caab9b
Jörg Schmidt <jroschmidt@gmail.com>
2018-05-20 22:32:11 +0200

    v3.0a3: Alpha 3 includes working scheduled operation scheme and is running and working during a first quick test.

---
+++ level-gauge_v3.0a3.py
@@ -0,0 +1,366 @@
+#!/usr/bin/python
+# Use Python 3
+# Written for the Raspberry Pi Zero W (2018) running Raspbian
+# Copyright 2018 Joerg R. Schmidt
+
+# Credit goes to Matt Hawkins from raspberrypi-spy.co.uk
+# Thanks for his tutorial on IoT using the RPi and ThingSpeak.
+# I reused the send_data() method to send data to Thingspeak
+
+import os
+import sys
+import resource
+import traceback
+import logging
+
+from enum import Enum
+
+import urllib  # URL functions
+import urllib2  # URL functions
+
+import time
+from time import sleep
+from datetime import datetime
+
+from gpiozero import LED
+from gpiozero import Button
+
+import scheduled_pump
+
+
+# Parameters ###########################################################################################################
+
+# Default minimum level for pump operation, in percent (Default = 20);
+# overwritten with user-specified value if possible
+threshold_default = 20
+# Width of hysteresis in percent (Default = 10)
+threshold_delta = 10
+# Frequency to check level, in seconds (Default = 30)
+sleep_time = 30
+
+THINGSPEAKKEY = 'C9Z5GCAFI42KFAQK'
+THINGSPEAKURL = 'https://api.thingspeak.com/update'
+
+log_file_path = 'logs'  # cannot be left empty
+log_file_name = ''   # prefix for log file name
+csv_file_name = ''   # prefix for csv file name
+
+
+# GPIO Pin Configuration
+SEED_VOLTAGE_PIN_NO = 26
+LVL_PIN_NO_ARRAY = [4, 17, 27, 22, 23, 24, 25, 5, 6, 13]    # ordered from 0 to 100
+PUMP_PIN_NO = 20
+STATUS_LED_PIN_NO = 21
+
+########################################################################################################################
+
+
+class ControlMode(Enum):
+    MANUAL = 1
+    SCHEDULED = 2
+    TIMED = 3
+
+
+# GLOBAL VARIABLES #####################################################################################################
+
+control_mode = ControlMode.SCHEDULED  # Start in scheduled mode by default
+
+
+# FUNCTION DEFINITIONS #################################################################################################
+
+
+# Get Timestamp
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+
+
+def print_and_log(message, log_file_path_abs):
+    print(message)
+    try:
+        log_file = open(log_file_path_abs, "a+")
+        message_string = "{}: {}\n".format(get_timestamp(), message)
+        log_file.write(message_string)
+        log_file.close()
+    except IOError as e:
+        print("Could not write to log file. IOError occurred: {}".format(e))
+
+
+def write_to_csv(message, csv_file_path_abs, log_file_path_abs):
+    try:
+        csv_file = open(csv_file_path_abs, "a+")
+        csv_file.write("{}\n".format(message))
+        csv_file.close()
+    except IOError as e:
+        print_and_log("Could not write to CSV file. IOError occurred: {}".format(e), log_file_path_abs)
+
+
+# Probe the (10) level pins for HIGH or LOW and return boolean array
+def probe_level_pins(lvl_pin_array, seed_voltage, log_file_path_abs):
+    lvl_pin_values = [False] * len(list(lvl_pin_array))
+
+    seed_voltage.on()
+
+    for x in range(len(lvl_pin_array)):
+        #print_and_log("Checking pin no. {}: GPIO {}".format(x, lvlPinNoArray[x]), log_file_path_abs)
+        lvl_pin_values[x] = lvl_pin_array[x].is_pressed
+
+    seed_voltage.off()
+    print_and_log("Level Pin status: {}".format(lvl_pin_values), log_file_path_abs)
+
+    return lvl_pin_values
+
+
+# First probe GPIO pins, then read the returned value array in reversed order (starting with 100%),
+# and break at first HIGH and return its corresponding level [0.0-1.0]
+def read_tank_level(lvl_pin_array, seed_voltage, log_file_path_abs):
+    lvl_pin_values_reversed = list(reversed(list(probe_level_pins(lvl_pin_array, seed_voltage, log_file_path_abs))))
+    lvl_pin_cnt = len(lvl_pin_array)
+
+    for x in range(lvl_pin_cnt + 1):
+        if x == lvl_pin_cnt:
+            break
+        if lvl_pin_values_reversed[x]:
+            break
+    result = (float(lvl_pin_cnt) - x) / float(lvl_pin_cnt)
+    print_and_log("Detected tank level is {}%".format(result * 100), log_file_path_abs)
+    return result
+
+
+# Turn on pump
+def pump_turn_on(pump, log_file_path_abs):
+    pump.on()  # Pump is ON when LOW
+    print_and_log("Pump turned ON", log_file_path_abs)
+    return True
+
+
+# Turn off pump
+def pump_turn_off(pump, log_file_path_abs):
+    pump.off()  # Pump is OFF when HIGH
+    print_and_log("Pump turned OFF", log_file_path_abs)
+    return False
+
+
+# Get textual representation of pump state
+def pump_state_textual(pump_state):
+    return "ON" if pump_state else "OFF"
+
+
+def send_data_to_thingspeak(url, key, level, pump_state, log_file_path_abs):
+    """
+  Send event to internet site
+  """
+
+    values = {'api_key': key, 'field1': level, 'field2': pump_state}
+
+    postdata = urllib.urlencode(values)
+    req = urllib2.Request(url, postdata)
+
+    try:
+        # Send data to Thingspeak
+        response = urllib2.urlopen(req, None, 5)
+        html_string = response.read()
+        response.close()
+
+        log_str = "Successfully sent data: {:.1f}, {:.2f}".format(level, pump_state) + ", Update " + html_string
+    except urllib2.HTTPError as e:
+        # Thrown when URL or Key are wrong
+        log_str = 'Error sending data to Thingspeak: Server could not fulfill the request. ' \
+                  'Error code: {}'.format(e.code)
+    except urllib2.URLError as e:
+        # Thrown when no internet connection
+        log_str = 'Error sending data to Thingspeak: Failed to reach server. Reason: {}'.format(e.reason)
+    except:
+        log_str = 'Unknown error sending data to Thingspeak'
+
+    print_and_log(log_str, log_file_path_abs)
+
+
+# TODO: Improve (settings file)
+def read_threshold_from_file(filepath, log_file_path_abs):
+    try:
+        cfg_file_threshold = open(filepath, "r")
+        threshold = int(cfg_file_threshold.readline())
+        cfg_file_threshold.close()
+        print_and_log("Successfully read threshold value from file: {}".format(threshold), log_file_path_abs)
+    except IOError as threshold_err:
+        print_and_log("IOError occurred: threshold could not be read. "
+                      "Using default value of {}. Error:\n{}"
+                      .format(threshold_default, threshold_err), log_file_path_abs)
+        return threshold_default
+    except ValueError as threshold_err:
+        print_and_log("Value Error occurred: threshold could not be read. "
+                      "Using default value of {}. Error:\n{}"
+                      .format(threshold_default, threshold_err), log_file_path_abs)
+        return threshold_default
+    return threshold
+
+
+# TODO: Replace, only for testing purposes
+def read_pump_on_off(filepath, log_file_path_abs):
+    try:
+        manctl_file = open(filepath, "r")
+        manctl = bool(int(manctl_file.readline()))
+        manctl_file.close()
+        print_and_log("Successfully read desired pump state from file: {}".format(pump_state_textual(manctl)), log_file_path_abs)
+    except IOError as threshold_err:
+        print_and_log("IOError occurred: threshold could not be read. "
+                      "Using default value of {}. Error:\n{}"
+                      .format(pump_state_textual(False), threshold_err), log_file_path_abs)
+        return False
+    except ValueError as threshold_err:
+        print_and_log("Value Error occurred: pump state could not be read. "
+                      "Using default value of {}. Error:\n{}"
+                      .format(pump_state_textual(False), threshold_err), log_file_path_abs)
+        return False
+    return manctl
+
+
+# MAIN FUNCTION ########################################################################################################
+def main():
+    now = datetime.now()
+    my_path = os.path.abspath(os.path.dirname(__file__))
+    log_file_path_abs = os.path.join(my_path, "{}/{}{}.log".format(log_file_path, log_file_name,
+                                                                    now.strftime("%Y-%m-%d_%H-%M-%S")))
+    csv_file_path_abs = os.path.join(my_path, "{}/{}{}.csv".format(log_file_path, csv_file_name,
+                                                                    now.strftime("%Y-%m-%d_%H-%M-%S")))
+    print_and_log("Starting...", log_file_path_abs);
+
+    write_to_csv("Time;Level;Pump", csv_file_path_abs, log_file_path_abs)
+
+    try:
+        # Set stack size limit to maximum possible
+        resource.setrlimit(resource.RLIMIT_STACK, (resource.RLIM_INFINITY, resource.RLIM_INFINITY))
+        print_and_log("Successfully increased stack size to maximum.", log_file_path_abs)
+    except Exception as e:
+        print_and_log("Unable to increase stack size. Reason: {}. Continuing...".format(e), log_file_path_abs)
+
+    # Create devices using GPIOZERO library
+    try:
+        pump = LED(PUMP_PIN_NO, active_high=False)
+        #pump_off(pump, log_file)
+        status_led = LED(STATUS_LED_PIN_NO)
+        seed_voltage = LED(SEED_VOLTAGE_PIN_NO)
+        lvl_pin_cnt = len(list(LVL_PIN_NO_ARRAY))
+        lvl_pin_array = [LED] * lvl_pin_cnt
+        for x in range(lvl_pin_cnt):
+            #print_and_log("Checking pin no. {}: GPIO {}".format(x, lvlPinNoArray[x]), log_file_path_abs)
+            lvl_pin_array[x] = Button(LVL_PIN_NO_ARRAY[x], pull_up=False)
+        print_and_log("Done initializing GPIO devices. Using {} level pins.".format(lvl_pin_cnt),
+                      log_file_path_abs);
+    except Exception as e:
+        print_and_log("Unable to create devices: {}. Exiting...".format(e), log_file_path_abs)
+        return 1
+
+    # CFG stuff - TODO: quick and dirty for testing, needs to be replaced
+    cfg_file_threshold_path_abs = os.path.join(my_path, "threshold.cfg")
+
+    # Manual pump ctl through file TODO: Replace
+    manctl_filepath = os.path.join(my_path, "manual_pump_control.cfg")
+
+    # Initialize pump state variable - pump is always off if not actively pulled LOW by program
+    pump_running = False
+
+    # TODO: Read Thingspeak URL and Key from config file
+
+    # Heartbeat TODO: check if this works as intended (when program crashes...)
+    status_led.blink()
+
+    # Switch to manual mode for testing TODO: Remove
+    # switch_mode(ControlMode.MANUAL)
+
+    # Main Loop
+    while True:
+        try:
+            # Read Level
+            level = read_tank_level(lvl_pin_array, seed_voltage, log_file_path_abs)
+
+            # Read Threshold from File
+            threshold = read_threshold_from_file(cfg_file_threshold_path_abs, log_file_path_abs)
+            print_and_log("Using threshold value of: {}".format(threshold), log_file_path_abs)
+
+            # Control pump
+            pump_allowed = is_pump_allowed(level, pump_running, threshold)
+            pump_desired = is_pump_desired(manctl_filepath, log_file_path_abs)
+            if pump_allowed & pump_desired:
+                pump_running = pump_turn_on(pump, log_file_path_abs)
+            else:
+                pump_running = pump_turn_off(pump, log_file_path_abs)
+
+            # Log Data
+            print_and_log("Current state: Level: {}% | Pump: [ALLOWED: {}, DESIRED: {}, RUNNING: {}]"
+                          .format(level * 100,
+                                  pump_allowed,
+                                  pump_desired,
+                                  pump_state_textual(pump_running)),
+                          log_file_path_abs)
+
+            write_to_csv("{};{};{}\n".format(get_timestamp(), level, int(pump_running)), csv_file_path_abs, log_file_path_abs)
+
+            # Send Data to Thingspeak
+            print_and_log("Sending data to Thingspeak...", log_file_path_abs)
+            send_data_to_thingspeak(THINGSPEAKURL, THINGSPEAKKEY, level * 100, int(pump_running), log_file_path_abs)
+            sys.stdout.flush()
+
+            # Wait until next check is due
+            print_and_log("Waiting {} seconds until next check...".format(sleep_time), log_file_path_abs)
+            sleep(sleep_time)
+        except Exception as err:
+            print_and_log("An unknown error occurred! Exiting...", log_file_path_abs)
+            print_and_log(traceback.format_exc(), log_file_path_abs)
+            raise err
+
+    # Terminate Program
+    print_and_log("Terminating...", log_file_path_abs)
+
+    print("Done.")
+    return 0
+
+
+def is_pump_allowed(level, pump_running, threshold):
+    """Determine if pump operation is allowed based on tank level"""
+    pump_allowed = pump_running
+    if pump_running:
+        if level * 100 < threshold:
+            # Level fell below lower threshold during pump operation, stop pumping
+            pump_allowed = False
+    else:
+        if level * 100 >= threshold + threshold_delta:
+            # Level is above upper threshold, we can start pumping if we want to
+            pump_allowed = True
+    return pump_allowed
+
+
+#TODO: Write logic here. Ultimately use WSGI / Django???
+def is_pump_desired(manctl_filepath, log_file_path_abs):
+    """Determine if pump operation is desired based on active control mode"""
+    global control_mode
+    if control_mode == ControlMode.MANUAL:
+        # read from file
+        pump_desired = read_pump_on_off(manctl_filepath, log_file_path_abs)
+    elif control_mode == ControlMode.SCHEDULED:
+        # read schedule from file and compare current weekday and time with schedule
+        pump_desired = scheduled_pump.is_pump_desired()
+    elif control_mode == ControlMode.TIMED:
+        #
+        pump_desired = True
+    else:
+        raise Exception("Something went terribly wrong. Pump should be turned off now.\n"
+                        "Exact Reason: current control mode not recognized.")
+
+    return pump_desired
+
+
+def switch_mode(desired_mode):
+    global control_mode
+    old_mode = control_mode
+    control_mode = desired_mode
+    print("Mode switched from {} to {}".format(old_mode, control_mode))
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logger = logging.getLogger()
+    try:
+        main()
+    except Exception as e:
+        logger.exception("Main crashed: %s\n%s", e, traceback.format_exc())
---
+++ scheduled_pump.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+# Use Python 3
+
+import csv
+from datetime import datetime
+
+
+def extract_time_windows_for_day(schedule_row):
+    window_cnt = int((len(schedule_row) - 1) / 2)
+    print("Found", window_cnt, "time windows for", schedule_row[0])
+    result = []
+    for window_i in range(1, window_cnt + 1):
+        window = [schedule_row[2 * (window_i - 1) + 1], schedule_row[2 * (window_i - 1) + 2]]
+        result.append(window)
+
+    return result
+
+
+def read_schedule():
+    """Read schedule from CSV file without (!) a header"""
+    try:
+        schedule_arr = []
+        with open('schedule.csv') as schedule_file:
+            schedreader = csv.reader(schedule_file, delimiter=',')
+            # next(schedreader)  # Skip header
+            # print("Rows read:")
+            for row in schedreader:
+                # print(row)
+                schedule_arr.append(row)
+            print("Successfully read schedule from CSV.")
+    except IOError:
+        print("IOError: " + schedule_file)
+
+    # Check schedule for integrity
+    for row in schedule_arr:
+
+        # Check incomplete windows (odd number of times)
+        if len(row) % 2 == 0:
+            raise Exception("Inconsistent schedule: A time window must have both start and end time. "
+                            "One is missing in {}'s schedule.".format(row[0]))
+
+        time_windows = extract_time_windows_for_day(row)
+
+        # More detailed checks
+        for window_i in range(0, len(time_windows)):
+            window_start_time = datetime.strptime(time_windows[window_i][0], "%H:%M").time()
+            window_end_time = datetime.strptime(time_windows[window_i][1], "%H:%M").time()
+
+            print("Time window ({}/{}) for today, {}: {} until {}"
+                  .format(window_i+1, len(time_windows), row[0],
+                          time_windows[window_i][0], time_windows[window_i][1]))
+
+            # Check for reversed window boundaries
+            if window_start_time > window_end_time:
+                raise Exception("Inconsistent schedule: End time of a window cannot be before its start time. "
+                                "Found in window number {} for {}'s schedule.".format(window_i+1, row[0]))
+
+            # Check for overlapping time windows
+            if window_i > 0:
+                prev_window_end_time = datetime.strptime(time_windows[window_i-1][1], "%H:%M").time()
+                if prev_window_end_time > window_start_time:
+                    raise Exception("Inconsistent schedule: Consecutive windows cannot overlap. "
+                                    "Found in window number {} for {}'s schedule.".format(window_i + 1, row[0]))
+
+    return schedule_arr
+
+
+def is_pump_desired():
+    """Check if we are within a specified time window of operation for the current weekday"""
+
+    # Read the schedule
+    schedule_arr = read_schedule()
+
+    # Determine what day of the week it is today
+    now = datetime.now()
+    weekday = now.isoweekday()-1
+    print("Today is", now.strftime("%A"))
+
+    # Extract the corresponding schedule for this day
+    schedule_today = schedule_arr[weekday]
+
+    # Extract the defined time windows for the day
+    time_windows_today = extract_time_windows_for_day(schedule_today)
+    window_cnt_today = len(time_windows_today)
+    print("Found", window_cnt_today, "time windows for", schedule_today[0])
+
+    # Check if we are within one of the time windows
+    for window in time_windows_today:
+
+        # Extract start and end time for current window
+        window_start_time = datetime.strptime(window[0], "%H:%M").time()
+        window_end_time = datetime.strptime(window[1], "%H:%M").time()
+
+        window_start_datetime = datetime.combine(now.date(), window_start_time)
+        window_end_datetime = datetime.combine(now.date(), window_end_time)
+
+        print("Today's hours of operation:)"
+              "{} until {}".format(window_start_time.strftime("%H:%M"), window_end_time.strftime("%H:%M")))
+        # print("window_start_datetime, window_end_datetime: ", window_start_datetime, window_end_datetime)
+
+        # Check if we are within the current time window
+        if (now >= window_start_datetime) & (now < window_end_datetime):
+            # We are within the current time window, return True
+            print("We ARE within a time window right now.")
+            return True
+
+    # We are not within one of the time windows for today, return False
+    print("We are NOT within a time window right now.")
+    return False
+
+
+def main():
+    # read_schedule()
+    print(is_pump_desired())
+    return 0
+
+
+if __name__ == "__main__":
+    main()
