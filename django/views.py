e850e0a1cedb23bbfef7e22385876ce3281b990a
Jörg Schmidt <jroschmidt@gmail.com>
2018-05-23 13:20:15 +0200

    v3.0b2: Mode switching through django app possible! (Changed mode enum definition)

--- django/views.py
+++ django/views.py
@@ -4,11 +4,14 @@ from django import forms
 
 import os
 
+
+mode_file_path = os.path.join("/home/pi/pumpcontrol", "cfg", "mode_selection.cfg")
+
+
 def read_mode_from_file():
     try:
-        filepath = os.path.join("/home/pi/pumpcontrol", "cfg", "mode_selection.cfg")
-        mode_file = open(filepath, "r")
-        mode = int(mode_file.readline())
+        mode_file = open(mode_file_path, "r")
+        mode = mode_file.readline()
         mode_file.close()
         print("Successfully read desired control mode from file: {}".format(mode))
     except IOError as mode_err:
@@ -24,24 +27,39 @@ def read_mode_from_file():
     return mode
 
 
+def write_mode_to_file(new_mode):
+    try:
+        mode_file = open(mode_file_path, "w")
+        mode_file.write(new_mode)
+        mode_file.close()
+        print("Successfully wrote new control mode to file: {}".format(new_mode))
+    except IOError as mode_err:
+        print("IOError occurred: control mode could not be written. Error:\n{}".format(mode_err))
+    except ValueError as mode_err:
+        print("Value Error occurred: control mode could not be written. Error:\n{}".format(mode_err))
+
+
 def index(request):
-    # if this is a POST request we need to process the form data
+    active_mode = read_mode_from_file()
     new_mode = ''
+
+    # if this is a POST request we need to process the form data
     if request.method == 'POST':
         form = forms.Form(request.POST)
         if request.POST.get('Manual'):
-            new_mode = "Manual"
-            pass  # do something
+            new_mode = "MANUAL"
         elif request.POST.get('Scheduled'):
-            new_mode = "Scheduled"
-            pass  # do something else
+            new_mode = "SCHEDULED"
+        elif request.POST.get('Timed'):
+            new_mode = "TIMED"
         else:
             pass  # unknown
 
+        write_mode_to_file(new_mode)
+
     # if a GET (or any other method) we'll create a blank form
     # else:
-    active_mode = read_mode_from_file()
-    # active_mode = "MANUAL"
+
     context = {'active_mode': active_mode, 'new_mode': new_mode}
 
     return render(request, 'frontend/index.html', context)
---
+++ pump_timer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# Use Python 3
+
+import csv
+from datetime import datetime
+
+
+class Timer():
+
+
+    def add_time(seconds):
+
+
+
+    def is_pump_desired(schedule_filepath):
+        """Check if time is over or not"""
+        return False
+
+
+    def main():
+        pump_desired = is_pump_desired('schedule.csv')
+        print(pump_desired)
+        return pump_desired
+
+
+if __name__ == "__main__":
+    main()
